283fed9842a0e5d5aa71216bf45e5e4d4ddc6dd7
tob <tobakucapitalism@gmail.com>
2015-07-21 07:47:07 +0900

    tests

--- R/helper.R
+++ R/helper.R
@@ -17,3 +17,16 @@ to_list <- function() make_visitor(
 to_call <- function() make_visitor(
   call = function(x) if(!is.pairlist(x)) as.call(x) else x
 )
+
+#' @rdname helper
+#' @export
+to_lisp <- function() make_visitor(
+    leaf = as.character
+  , call = function(x) as_char(x)
+  , vars = list(
+      as_char = function(x) {browser();paste0("(", hd(x), " ", tl(x[-1]), ")")}
+    , hd = function(x) as.character(x[[1]])
+    , tl = function(x) paste0(lapply(x, function(k) if (length(k) == 1) as.character(k) else as_char(x)), collapse = " ")
+    , last = noquote
+  )
+)
