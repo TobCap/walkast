77e91ddf114863f5cb5c4f77f9b063cce6ccb01c
tob <tobakucapitalism@gmail.com>
2015-07-23 19:08:02 +0900

    simplify to_lisp() and to_r() along with a change of walk_ast()

--- R/helper.R
+++ R/helper.R
@@ -4,6 +4,8 @@
 #' walk_ast(quote(1 + 2 * 3), to_list())
 #' walk_ast(list(quote(`+`), 1, list(quote(`*`), 2, 3)), to_call())
 #' walk_ast(quote(1 + 2 * 3), to_list() %then% to_call())
+#' walk_ast(quote(1 + 2 * 3), to_lisp())
+#' walk_ast(quote(1 + 2 * 3), to_r())
 NULL
 
 #' @rdname helper
@@ -21,23 +23,17 @@ to_call <- function() make_visitor(
 #' @rdname helper
 #' @export
 to_lisp <- function() make_visitor(
-    leaf = function(x) as.character(deparse(x))
-  , call = function(x) as_char(x)
-  , vars = list(
-      as_char = function(x) if (length(x) <= 1) hd(x) else paste0("(", hd(x[[1]]), " ", tl(x[-1]), ")")
-    , hd = function(x) as.character(x)
-    , tl = function(x) paste0(lapply(x, as_char), collapse = " ")
-    , last = noquote
-  )
+    call = function(x) paste0(x, collapse = "")
+  , hd = function(x) paste0("(", if (length(x) <= 1) as.character(list(x)) else tl(x), " ")
+  , tl = function(x) paste0(paste0(x, collapse = " "), ")", collapse = "")
+  , last = noquote
 )
 
+#' @rdname helper
+#' @export
 to_r <- function() make_visitor(
-    leaf = function(x) as.character(list(x))
-  , call = function(x) as_char(x)
-  , vars = list(
-      as_char = function(x) if (length(x) <= 1) hd(x) else paste0(hd(x[[1]]), "(", tl(x[-1]), ")")
-    , hd = function(x) as.character(list(x[[1]]))
-    , tl = function(x) paste0(lapply(x, as_char), collapse = ", ")
-    , last = noquote
-    )
+    call = function(x) paste0(x, collapse = "")
+  , hd = function(x) if (length(x) <= 1) as.character(list(x)) else tl(x)
+  , tl = function(x) paste0("(", paste0(x, collapse = ", "), ")", collapse = "")
+  , last = noquote
 )
